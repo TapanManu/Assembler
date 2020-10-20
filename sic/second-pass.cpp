7ec984ed553b4c83a5f2e978c9cecec4cc19df31
TapanManu <tapanmanu2000@gmail.com>
2020-10-21 00:55:42 +0530

    obj code conversion perfect

--- sic/second-pass.cpp
+++ sic/second-pass.cpp
@@ -4,13 +4,53 @@
 
 using namespace std;
 
-char obj[10],opc[10],addr[10],operand[10];
+char obj[10],opc[10],addr[10],operand[10],val[20];
 char err[] = "error";
+
+stack <int> s;
+
+char* substring(char* dest,char* source,int min,int max){
+    int j=0;
+    for(int i=min;i<max;i++){
+        dest[j++]=source[i];
+    }
+    dest[j++]='\0';
+    return dest;
+}
+
+void decToHex(int ctr){
+    while(ctr>0){
+        char ch=65;
+        int d = ctr%16;
+        if(d>9 && d<16){
+            ch = int(ch) + d - 10;
+        }
+        else{
+            ch = d;
+        }
+        s.push(int(ch));
+        ctr/=16;
+    }
+    int i=0;
+    while(!s.empty()){
+        if(s.top()>=65 && s.top()<=90){
+            
+            val[i]=(char)s.top();
+            i++;
+        }
+        else{
+            val[i]=s.top()+48;      //char equivalent of num
+            i++;
+        }
+        s.pop();
+    }
+    val[i++]='\0';
+}
 
 int charToInt(char str[],int x){
     static int len = x+1;
     if(x<0)
-        return 0;                   //doubt.........
+        return 0;                   
     return ((str[x])-'0')*pow(10,len-x-1) + charToInt(str,--x);
 }
 char* translate_opcode(char* op){
@@ -52,7 +92,7 @@ char* translate_oper(char* op,int x){
                 strcpy(addr,zero);
             }
             if(x==1){
-                addr[2]=index_addr(addr[2]);
+                addr[0]=index_addr(addr[0]);
             }
             return addr;
         }
@@ -62,7 +102,7 @@ char* translate_oper(char* op,int x){
 }
 
 void objcode(char* line){
-    char opcode[20],locctr[10],operand[10];
+    char opcode[20],locctr[10],operand[10],opr[10];
     int x = 0;
     char zero[2]="0";
     sscanf(line,"%s\t%s\t%s\t",locctr,opcode,operand);
@@ -72,7 +112,9 @@ void objcode(char* line){
     if(operand[len-2]==','){            //whether index addressing used
         if(charToInt(operand,len-1)>1){
             x = 1;
+            substring(opr,operand,0,strlen(operand)-2);
         }
+        strcpy(operand,opr);
     }
     char obj[25],oper[25];
     strcpy(obj,translate_opcode(opcode));
@@ -94,12 +136,36 @@ void objcode(char* line){
                 strcpy(oper,"    ");
             }
             if(!strcmp(opcode,"WORD")||!strcmp(opcode,"BYTE")){
+                if(!strcmp(opcode,"BYTE")){
+                    if(operand[0]=='C' && operand[1]=='`'){
+                        int i=2;
+                        strcpy(oper,"");
+                        while(operand[i]!='\0'){
+                            int c = operand[i];
+                            strcpy(val,"");
+                            decToHex(c);
+                            strcat(oper,val);
+                            i++;
+                        }
+                    }
+                    if(operand[0]=='X' && operand[1]=='`'){
+                       strcpy(oper,substring(oper,operand,2,strlen(operand)));
+                    }
+                    while(strlen(oper)<4){
+                        strcpy(zero,"0");
+                        strcat(zero,oper);
+                        strcpy(oper,zero);
+                    }
+                    strcpy(oper,operand);
+                }
+                else{
                 while(strlen(operand)<4){
                     strcpy(zero,"0");
                     strcat(zero,operand);
                     strcpy(operand,zero);
                 }
-            strcpy(oper,operand);
+                strcpy(oper,operand);
+                }
             }
         }
         cout<<oper<<"\n";
@@ -115,6 +181,7 @@ int main(){
         char line[25];
         strcpy(line,s.c_str());
         objcode(line);
+        /* obj code perfect correction now create object file*/
     }
     return 0;
 }
