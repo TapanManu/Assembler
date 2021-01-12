0c797fa15bd36722451b40d774bf6a970d9d97c5
TapanManu <tapanmanu2000@gmail.com>
2021-01-12 20:58:24 +0530

    expand and define functions

--- macro/macro.c
+++ macro/macro.c
@@ -4,17 +4,35 @@
 #include <stdlib.h>
 void expand();
 void define(FILE*,char[],char[]);
-void read_namtab(char[]);
+int read_namtab(char[]);
 
 int expanding = 0;
+int arglen=0;               //argtab length
 int macro_index = 0;
-int defloc = 0;         //location ponter in deftab
+int defloc = 0;
+int exploc=0;         //location ponter in deftab
 int NAMTAB[10][2];
 char ARGTAB[10][15];    //max no of args allowed in macro = 10
-char* pgmname;
+char* pgmname, *sub;
 char label[10],opcode[10],operand[10];
 int found = 0;
+int err_flag;
 char* defline;
+
+char* substr(char* str,int beg,int end){
+    int len = end - beg + 1;
+    char* temp;
+    temp = (char*)malloc(len);
+    sub = (char*)malloc(len);
+    int i = beg;
+    int j=0;
+    while(i < end){
+       temp[j++] = str[i++];
+    }
+    temp[j]='\0';
+    strcpy(sub,temp);
+    return sub;
+}
 
 void clear_src(){
     FILE *fp;
@@ -48,6 +66,7 @@ void write_source(char label[],char opcode[],char operand[]){
 void get_line(){
     if(expanding == 1){
         //read deftab
+        
         //substitute argtab for positional notation
     }
     return;
@@ -57,7 +76,7 @@ void processline(FILE* fp,char label[],char opcode[],char operand[]){
     found = 0;
     read_namtab(opcode);        //  search for macro name
     if(found==1){
-        printf("\tMACRO\n");
+        //printf("\tMACRO\n");
         expand();
     }
     else if(!strcmp(opcode,"MACRO")){
@@ -82,7 +101,7 @@ void read_deftab(){
 }
 
 
-void read_namtab(char opcode[]){
+int read_namtab(char opcode[]){
     FILE *fp;
     fp = fopen("namtab.txt","r");
     char lin[255],*macro_name;
@@ -90,14 +109,15 @@ void read_namtab(char opcode[]){
     macro_name = (char*)malloc(15);         //max macro name size == 15
 
     while(fgets(lin,255,fp)){
-        sscanf(lin,"%d %s %d %d",&macro_index,macro_name,&start_indx,&end_indx);
+        sscanf(lin,"%d %s",&macro_index,macro_name);
        // printf("%s",macro_name);        
         if(!strcmp(opcode,macro_name)){
             found = 1;
+            return macro_index;
         }
 
     }
-
+    return -1;
     fclose(fp);
 }
 
@@ -114,13 +134,43 @@ void create_argtab(char args[]){
             ARGTAB[argcount][k] = '\0';
             continue;
         }
-        else if(i==(strlen(args)-1)){
+        else if(i==(strlen(args)-1) || args[i]=='\0'){
             ARGTAB[argcount][k] = '\0';
         }
         else{
             ARGTAB[argcount][k++] = args[i];
         }
     }
+    arglen = argcount;
+}
+
+void sub_argtab(char args[]){
+    int i=0;
+    int argcount=0;
+    int k=0;
+    for(i=0;i<strlen(args);i++){
+        if(args[i]==','){
+            ARGTAB[argcount][k] = '\0';
+            argcount++;
+            k=0;
+        }
+        else if(i==(strlen(args)-1) || args[i]=='\0'){
+            ARGTAB[argcount][k] = '\0';
+        }
+        else{
+            ARGTAB[argcount][k++] = args[i];
+        }
+    }
+    arglen = argcount;
+}
+
+int search_argtab(char arg[]){
+    for(int i=0;i<arglen;i++){
+        if(!strcmp(ARGTAB[i],arg)){
+            return i+1;
+        }
+    }
+    return -1;      //if arg not present
 }
 
 void insert_namtab(char name[]){
@@ -134,7 +184,10 @@ void insert_namtab(char name[]){
 }
 
 void expand(){
-    expanding = 1;      
+    expanding = 1;
+    int br=0; 
+    int length = 0;
+    char arg[15];     
     FILE *fp;
     fp = fopen("deftab.txt","r");       //read deftab
     char lab[10],opc[10],oper[10];
@@ -142,15 +195,22 @@ void expand(){
     fgets(defline,255,fp);
     lab[0]=0; opc[0]=0; oper[0]=0;
     sscanf(defline,"%s %s %s",lab,opc,oper);
-    
+
     //set up argtab , map posiitonal index
+
+    sub_argtab(oper);
+    char dot[]=".\t";
+    strcat(dot,defline);
+    strcpy(defline,dot);
+    write_src(defline);
+    
     //macro invocation to expanded file as comment
     while(strcmp(opc,"MEND")){
         lab[0]=0; opc[0]=0; oper[0]=0;
         fgets(defline,255,fp);
         sscanf(defline,"%s %s %s",lab,opc,oper);
         if(lab[0]=='.'){
-            write_source(lab,opc,oper);
+            write_src(defline);
             continue;
         }
         if(oper[0]==0){
@@ -167,18 +227,96 @@ void expand(){
 
 void define(FILE *fp,char label[],char oper[]){
     insert_namtab(label);
-    char defline[255];
+    char* mtab;
+    mtab = (char*)malloc(20);
+   
+    char defline[255],lab[20],opc[20],oper[20];
     fgets(defline,255,fp);
     insert_deftab(defline);
     create_argtab(oper);        //creating variable argument list
+    defloc++;
     int level=1;
     while(level>0){
         fgets(defline,255,fp);
+        if(!strcmp(defline,"")){
+            err_flag = 1;
+            return;
+        }
         if(defline[0]=='.'){
             write_src(defline);     //comments
+            continue;
         }
-        //search for positional param
-        
+        lab[0]=0; opc[0]=0; oper[0]=0;
+        sscanf(defline,"%s %s %s",lab,opc,oper);
+        char arg[15];
+        int j=0,cnt=0;
+        int replacepos=-1;
+        for(int i=0;i<strlen(defline);i++){
+            if(cnt == 1){         //start counting fro arguments
+                if(isalnum(defline[i])){
+                    arg[j] = defline[i];
+                    if(j==0){
+                        replacepos = i;
+                    }
+                    else{
+                        defline[i]=' ';
+                    }
+                    j++;
+                }
+                else if(i == strlen(defline)-1){
+                    j = 0;
+                    cnt = 0;
+                    //compare it with argtab
+                    int s = search_argtab(arg);
+                    if(s==-1){
+                        err_flag = 2;   //argument not found
+                    }
+                     //replace the position value
+                    defline[replacepos] = s + '0';
+                }
+                else{
+                    j = 0;
+                    cnt = 0;
+                    //compare it with argtab
+                    int s = search_argtab(arg);
+                    if(s==-1){
+                        err_flag = 2;   //argument not found
+                    }
+                     //replace the position value
+                    defline[replacepos] = s + '0';
+                }
+            }
+            if(defline[i]=='&'){
+                //search for positional param
+                //locate param by begining with &
+                cnt = 0;
+                defline[i]='?';
+            }
+        }
+        insert_deftab(define);      //insert into definition table
+        defloc++;
+        if(oper[0]==0){
+            strcpy(oper,opc);
+            strcpy(opc,lab);
+            strcpy(lab,"");
+        }
+        if(!strcmp(opc,"MACRO")){
+            strcpy(mtab,lab);
+            level+=1;
+        }
+        if(!strcmp(opc,"MEND")){
+            
+            level-=1;
+            if(level==0){
+                int index = read_namtab(label);
+                NAMTAB[index][1] = defloc;
+            }
+            else{
+                int index = read_namtab(mtab);
+                NAMTAB[index][1] = defloc;
+            }
+            
+        }
     }
 
 }
